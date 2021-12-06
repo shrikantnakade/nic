b0654bc513d59e2e72b0af6b56d5729d28c59d2c
shrikant.r4909@gmail.com <shrikant,r4909@gmail.com>
2021-12-06 17:06:17 +0530

    changed web.php file

--- app/Modules/ResidentialSurvey/routes/web.php
+++ app/Modules/ResidentialSurvey/routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('residential-survey', 'ResidentialSurveyController@welcome');
+Route::get('residential', 'ResidentialSurveyController@welcome');
